52403c5295cdc84a066157671cdb134e9a6a59e0
KevinDb123 <2381538829@qq.com>
2024-10-13 09:28:33 +0800

    remove somethings

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import  models
-#创建模型#
-class Topic(models.Model):
-    text=models.charField(max_length=200)
-    date_added=models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.text
