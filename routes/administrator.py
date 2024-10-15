1e064cf68b4499d55e5bfa2c75f5e582e2b76d3f
KevinDb123 <2381538829@qq.com>
2024-10-15 23:08:40 +0800

    修改

--- routes/administrator.py
+++ routes/administrator.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for,g
+from flask import Blueprint, render_template, request, redirect, url_for,g,render_template_string
 from models import User
 from exts import db
 
@@ -13,14 +13,27 @@ def UserLists():
 #管理员给用户修改密码
 @admin_bp.route("/Admin_repwd/<username>",methods=['GET'])
 def Admin_repwd(username):
-    print(1)
     return render_template("Admin_repwd.html",username=username)
 @admin_bp.route("/Admin_repwd/<username>",methods=['POST'])
 def Admin_rep(username):
-    username = request.args.get('username')
     new_password=request.form.get("newPassword")
     user1=User.query.filter_by(username=username).first()
     if user1:
         user1.password = new_password
         db.session.commit()
+    return redirect(url_for('admin.UserLists'))
+# 删除用户
+@admin_bp.route("/Admin_deleteUser/<username>")
+def Admin_deleteUser(username):
+    user1=User.query.filter_by(username=username).first()
+    if user1 and username!="KevinDb123":
+        db.session.delete(user1)
+        db.session.commit()
+    elif user1.username=="KevinDb123":
+        return render_template_string('''
+                <script>
+                    alert('管理员账号不得注销');
+                    window.location.href = "{{ url_for('admin.UserLists') }}";
+                </script>
+            ''')
     return redirect(url_for('admin.UserLists'))
