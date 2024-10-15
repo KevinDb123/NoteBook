e2082bff0b34bd78a90c4fd349d34606599dc566
KevinDb123 <2381538829@qq.com>
2024-10-15 19:56:29 +0800

    1

--- routes/administrator.py
+++ routes/administrator.py
@@ -10,3 +10,17 @@ def UserLists():
     users=User.query.all()
     user_lists=[user.to_dict() for user in users]
     return render_template("UserLists.html",users=user_lists)
+#管理员给用户修改密码
+@admin_bp.route("/Admin_repwd/<username>",methods=['GET'])
+def Admin_repwd(username):
+    print(1)
+    return render_template("Admin_repwd.html",username=username)
+@admin_bp.route("/Admin_repwd/<username>",methods=['POST'])
+def Admin_rep(username):
+    username = request.args.get('username')
+    new_password=request.form.get("newPassword")
+    user1=User.query.filter_by(username=username).first()
+    if user1:
+        user1.password = new_password
+        db.session.commit()
+    return redirect(url_for('admin.UserLists'))
