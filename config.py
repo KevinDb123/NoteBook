841f304d6e2185fea74da081cef41faf90c358a1
Li siyuan <152281654+KevinDb123@users.noreply.github.com>
2025-02-14 11:27:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     HOSTNAME = "localhost"
-    PORT = '3306'
-    USERNAME = "root"
-    PASSWORD = "Tmz_20200212_zmT"
+    PORT = 'port'#需修改
+    USERNAME = "username"#需修改
+    PASSWORD = "password"#这里进行了修改
     DATABASE = "online_notebook"
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}'
